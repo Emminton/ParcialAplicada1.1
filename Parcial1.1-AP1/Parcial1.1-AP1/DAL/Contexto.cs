ce1fa91be27a13e733f45c89f14136c2047fe357
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2019-09-25 15:43:49 -0400

    Carpetas creada

---
+++ Parcial1.1-AP1/Parcial1.1-AP1/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Parrcial1_AP.BLL;
+using Parrcial1_AP.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parrcial1_AP.DAL
+{
+    public class Contexto : DbContext
+    {  
+            public DbSet<Estudiantes> estudiante { get; set; }
+            public Contexto() : base("ConStr")
+            {
+
+            }
+    }
+    
+}
